84d3d3b302602c541a854e3cdb2a0d0492a0536b
sabbir00921 <sabbir00921@gmail.com>
2024-12-13 00:57:06 +0600

    Check_Positive,_Negative,_or_Zero

---
+++ Check_Positive,_Negative,_or_Zero/js/main.js
@@ -0,0 +1,10 @@
+const num = +prompt("Enter a number");
+if(num>0){
+    console.log(`${num} is positive number`)
+}
+else if(num<0){
+    console.log(`${num} is negative number`)
+}
+else{
+    console.log(`Input number is Zero`)
+}
