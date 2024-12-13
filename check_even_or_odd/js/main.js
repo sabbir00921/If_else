6580b9a0280f3756692cfed8a6acbe23fd401d22
sabbir00921 <sabbir00921@gmail.com>
2024-12-14 02:38:35 +0600

    Check_even_or_odd

---
+++ check_even_or_odd/js/main.js
@@ -0,0 +1,9 @@
+//Check even or odd
+const num = 46;
+if(num %2 == 0)//This condition check the devisor is zero
+{
+    console.log("The number is even");
+}
+else{
+    console.log("The number is odd");
+}
